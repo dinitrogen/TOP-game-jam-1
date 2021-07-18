d02779690d3c4ecf56e6228fe1d9dee223011bc3
Kevin Handy <kevin@kevins-mbp.lan>
2021-07-18 09:05:14 -0400

    added hit detection for hero and enemy, and moved notes and maps to external files

---
+++ maps.js
@@ -0,0 +1,27 @@
+let maps = [
+    {
+        id: 1,
+        walls: [
+            {
+                type: 'vertical',
+                length: 4,
+                startX: 3,
+                startY: 2
+            },
+            {
+                type: 'horizontal',
+                length: 4,
+                startX: 4,
+                startY: 1
+            },
+            {
+                type: 'vertical',
+                length: 3,
+                startX: 7,
+                startY: 5
+            }
+        ]
+    }
+];
+
+export { maps }
